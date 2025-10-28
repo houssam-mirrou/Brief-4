d5a2d627793b1a42ebb852ca1764681441bc0491
houssam-mirrou <hossam.mirrou@gmail.com>
2025-10-28 22:37:44 +0100

    fixer des bug dans le premier jeux et reecriture du code

--- game_1/java_script.js
+++ game_1/java_script.js
@@ -1,80 +1,75 @@
 const container = document.querySelector(".container");
 const difficultty = document.getElementById("difficulty");
 const butt = document.getElementById("play-button");
-const card = document.getElementById("card");
-
 
 butt.addEventListener("click", function () {
     let diff = Number(difficultty.value);
-    console.log(typeof diff);
-    let cpt = 1;
-    while (container.firstChild) {
+    let total_number = diff*diff;
+    let i;
+    while (container.firstChild){
         container.removeChild(container.firstChild);
     }
-
-    let list = new Array(diff * diff);
-    let k = diff * diff;
-
-    for (let i = 0; i < k; i++) {
-        let card = document.createElement("div");
-        if (i == k / 2) {
-            cpt = 1;
+    let table = [];
+    let j=1;
+    for(i=0;i<total_number;i++){
+        if(i === (total_number/2)){
+            j=1;
         }
-        card.setAttribute("class", "card");
+        table[i]=j++;
+    }
+    
+    for(i=0 ; i<total_number;i++){
+        j = Math.floor(Math.random() * (total_number)+1);
+        let temp =table[i];
+        table[i]=table[j];
+        table[j]=temp;
+    }
+
+    let firstCard=null;
+    let secondCard=null;
+    for(i=0;i<total_number;i++){
+        let card = document.createElement("div");
+        card.className = "card";
+
         let text = document.createElement("h2");
-        text.setAttribute("class", "number");
-        text.textContent = cpt++;
+        text.className="number";
+        text.textContent=table[i];
+        text.style.display="none";
         card.appendChild(text);
-        list[i] = card;
-    }
-    for (let i = 0; i < (diff * diff); i++) {
-        let j = (Math.floor(Math.random() * (diff * diff)));
-        let temp = list[i];
-        list[i] = list[j];
-        list[j] = temp;
-    }
 
-    let number_1 = 1;
-    let number_2;
-    let number_3;
-
-    for (let i = 0; i < (diff * diff); i++) {
-        list[i].addEventListener("click", function () {
-            if (number_1 % 2 === 0) {
-                let val1 = list[i].querySelector(".number").textContent.replace("card turned ", "");
-                let val2 = list[number_2].querySelector(".number").textContent.replace("card turned ", "");
-                if (val1 !== val2) {
-                    let trim = list[number_2].textContent;
-                    trim = trim.substring(12, (list[number_2].textContent.length));
-                    list[number_2].textContent = trim;
-                    console.log("not-equals");
-
-                }
-                else if (("card turned " + list[i].textContent) === (list[number_2].textContent)) {
-                    list[i].textContent = "card turned " + list[i].textContent;
-                    console.log("equals");
-
-                }
+        card.addEventListener("click",function(){
+            let numberElem = this.querySelector(".number");
+            numberElem.style.display="block";
+            this.style.pointerEvents="none";
+            if(firstCard==null){
+                firstCard=this;
             }
-            else {
-                turn_card(list[i], list[i].textContent);
-                number_2 = i;
+            else{
+                secondCard=this;
+                checkMAtch();
             }
+        });
+        container.appendChild(card);
 
-            number_1++;
-        })
     }
 
-    for (let card of list) {
-        container.append(card);
+    function checkMAtch() {
+    let val1 = firstCard.querySelector(".number").textContent;
+    let val2 = secondCard.querySelector(".number").textContent;
+    if(val1 === val2){
+        firstCard=null;
+        secondCard==null;
     }
+    else {
+        setTimeout(function() {
+            firstCard.querySelector(".number").style.display="none";
+            secondCard.querySelector(".number").style.display="none";
+            firstCard.style.pointerEvents="auto";
+            secondCard.style.pointerEvents="auto";
+            firstCard=null;
+            secondCard=null;
+        },700);
+    }
+}
 });
 
-
-function turn_card(item, i) {
-    item.innerHTML = "";
-    let text = document.createElement("h2");
-    text.setAttribute("class", "number");
-    text.textContent = "card turned " + i;
-    item.appendChild(text);
-}
