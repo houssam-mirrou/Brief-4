98c553318baff532a573b44850e598ef1013688c
houssam-mirrou <hossam.mirrou@gmail.com>
2025-10-31 16:03:19 +0100

    fixer les bug de deuxiem jeux et l'ajout des statistiques

--- game_2/jsfile.js
+++ game_2/jsfile.js
@@ -1,50 +1,77 @@
 const button = document.querySelector(".button");
-const text_area = document.querySelector("#textarea");
 const timer = document.querySelector(".timer");
-const best_time = document.querySelector(".best_time");
+const accuracy = document.querySelector(".accuracy");
 const timer_input = document.querySelector("#timer_input");
 const text_holder = document.querySelector("#text_holder");
-
-button.addEventListener("click", function () {
-    let text = "";
-
-    fetch("text.txt")
-        .then(function (response) {
-            return response.text();
-        })
-        .then(function (data) {
-            text = data;
-            text_holder.textContent = text;
-            text_area.addEventListener("input", function () {
-                const text_value = text_area.value.trim();
-
-                //contains all the word written in the textarea
-                const words = text_value.split(/\s+/).filter(Boolean);
- 
-                //contains the last word in the text area written
-                const lastWord = words[words.length - 1];
-                const text_holder_words = text_holder.textContent.split(/\s+/).filter(Boolean);
-                let i=0;
-                for(let word of words){
-                    if(text_holder_words[i] == lastWord){
-                        console.log("equal");
-                        console.log(lastWord);
-                        text_area.style.color = "green";
-                    }
-                    else {
-                        text_area.style.color = "red";
-                    }
-                    i++;
-                }
-            })
+const textarea_container = document.querySelector(".textarea-container");
+const wpm = document.querySelector(".wpm");
+const errors = document.querySelector(".errors");
 
 
+text_holder.style.height = "30vh";
 
-        })
-        .catch(function (err) {
-            console.error(err);
-        });
+let wpm_value = 0;
+let accuracy_value = 0;
+let span_arr = new Array();
+let number_of_words = 0;
+let error_number = 0;
+let string = "";
+let k = 0;
+
+function reset_the_game() {
+    string = "";
+    span_arr = [];
+    while (text_holder.firstChild) {
+        text_holder.removeChild(text_holder.firstChild);
+    }
+    text_holder.setAttribute("tabindex", "0");
+    text_holder.focus();
+    timer.textContent = "";
+    text_holder.style.height = "auto";
+}
+
+function createSpans(text) {
+    for (let i = 0; i < text.length; i++) {
+        let span = document.createElement("span");
+        span.textContent = text[i];
+        span.style.opacity = 0.7;
+        span_arr.push(span);
+        text_holder.appendChild(span);
+    }
+}
+
+function key_pressed(event) {
+    if (event.key == 'Backspace') {
+
+        if (k > 0) {
+            k--;
+            span_arr[k].style.color = "white";
+            span_arr[k].style.opacity = 0.7;
+            string = string.slice(0, -1);
+        }
+        return;
+    }
+    if (event.key == span_arr[k].textContent) {
+        span_arr[k].style.color = "green";
+        span_arr[k].style.opacity = 1;
+        string = string + event.key;
+        k++;
+    }
+    else if (event.key == 'CapsLock' || event.key == 'Tab' || event.key == 'Control' || event.key == 'Alt') {
+        return;
+    }
+    else {
+        error_number++;
+        span_arr[k].style.color = "red";
+        span_arr[k].style.opacity = 1;
+        string = string + event.key;
+        k++;
+    }
+}
+
+function start_timer() {
     let timeleft = Number(timer_input.value);
+    timeleft--;
     button.disabled = true;
     function updateTimer() {
         let min = Math.floor(timeleft / 60);
@@ -53,6 +80,20 @@ button.addEventListener("click", function () {
             clearInterval(timerInterval);
             timer.textContent = "Time's up";
             button.disabled = false;
+            text_holder.removeAttribute("tabindex");
+            accuracy_value = 0;
+            for (let j = 0; j < span_arr.length; j++) {
+                if (span_arr[j].style.color == "green") {
+                    accuracy_value++;
+                }
+            }
+            accuracy_value = (accuracy_value * 100) / span_arr.length;
+            accuracy.textContent = accuracy_value.toFixed(2) + "%";
+            let string_arr = string.split(/\s+/);
+            wpm_value = string_arr.length;
+            wpm.textContent = wpm_value;
+            errors.textContent = error_number;
+
         } else if (min <= 0) {
             if (sec < 10) {
                 timer.textContent = "0" + sec;
@@ -71,9 +112,25 @@ button.addEventListener("click", function () {
         timeleft--;
     }
     const timerInterval = setInterval(updateTimer, 1000);
+    k = 0;
+}
 
+button.addEventListener("click", function () {
+    reset_the_game();
+    fetch("text.txt")
+        .then(function (response) {
+            return response.text();
+        })
+        .then(function (data) {
+            text = data;
+            createSpans(text);
+            text_holder.addEventListener("keydown", key_pressed);
+        })
+        .catch(function (err) {
+            console.error(err);
+        });
 
-
+    start_timer();
 
 });
 
